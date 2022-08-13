86acca9282b5409d45efc5813f04d26fb4315970
JOSUE RAFAEL GOMEZ DIAZ <SMSS108122@ugb.edu.sv>
2022-08-13 14:28:30 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("ingrese un numero entero (con 0 se finaliza)");
+                num = int.Parse(Console.ReadLine());
+
+                if (num >= 10) ;
+                {
+                    Console.WriteLine("tiene 3 digitos");
+                }
+                else if (num >= 10)
+                {
+                    Console.WriteLine("tiene 2 digitos");
+                }
+                else
+                {
+                    Console.WriteLine("tiene 1 digito");
+                }
+            } while (num != 0);
+
+
+
+        }
+    }
+}
